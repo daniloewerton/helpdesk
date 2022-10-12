2bd6b7ae1fb2295d8e4608b22a0939b503d501e4
daniloewerton <danilo.ewe@gmail.com>
2022-10-12 10:55:15 -0300

    CRUD para Cliente implementado

---
+++ src/main/java/com/daniloewerton/helpdesk/services/ClienteService.java
@@ -0,0 +1,71 @@
+package com.daniloewerton.helpdesk.services;
+
+import com.daniloewerton.helpdesk.domain.Cliente;
+import com.daniloewerton.helpdesk.domain.Pessoa;
+import com.daniloewerton.helpdesk.domain.Tecnico;
+import com.daniloewerton.helpdesk.domain.dtos.ClienteDTO;
+import com.daniloewerton.helpdesk.domain.dtos.TecnicoDTO;
+import com.daniloewerton.helpdesk.repositories.ClienteRepository;
+import com.daniloewerton.helpdesk.repositories.PessoaRepository;
+import com.daniloewerton.helpdesk.repositories.TecnicoRepository;
+import com.daniloewerton.helpdesk.services.exceptions.DataIntegrityViolationException;
+import com.daniloewerton.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repository;
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public Cliente findById(Integer id) {
+        Optional<Cliente> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+    }
+
+    public List<Cliente> findAll() {
+        return repository.findAll();
+    }
+
+    public Cliente create(ClienteDTO objDTO) {
+        objDTO.setId(null);
+        validaPorCpfEEmail(objDTO);
+        Cliente newObj = new Cliente(objDTO);
+        return repository.save(newObj);
+    }
+
+    public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+        objDTO.setId(id);
+        Cliente oldObj = findById(id);
+        validaPorCpfEEmail(objDTO);
+        oldObj = new Cliente(objDTO);
+        return repository.save(oldObj);
+    }
+
+    public void delete(Integer id) {
+        Cliente obj = findById(id);
+        if(obj.getChamados().size() > 0) {
+            throw new DataIntegrityViolationException("Cliente possui ordens de serviço e não pode ser deletado!");
+        }
+        repository.deleteById(id);
+    }
+
+    private void validaPorCpfEEmail(ClienteDTO objDTO) {
+        Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+        if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+            throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+        }
+
+        obj = pessoaRepository.findByEmail(objDTO.getEmail());
+        if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+            throw new DataIntegrityViolationException("Email já cadastrado no sistema!");
+        }
+    }
+}
